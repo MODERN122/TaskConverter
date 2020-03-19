e808f7c3671ce3199389c8cd5271133f4db5be1d
Михаил <filmih24@mail.ru>
2020-03-19 19:03:36 +0400

    update

---
+++ AgeToColorConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace WpfAppTest
+{
+    public class AgeToColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) =>
+            (value is int age)
+                ? age < 18
+                    ? Color.FromRgb(255,0,0).ToString()
+                    : Color.FromRgb(0, 255, 0).ToString()
+                : Color.FromRgb(0,0,0).ToString();
+        //if need convertBack maybe add age to color like color.FromRgb(0,255,(byte)age))
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) =>
+            (value is Color colorPerson)
+                ? colorPerson != Color.FromArgb(0,0,0,0)
+                    ? (int?)colorPerson.B
+                    : null
+                : null;
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfAppTest
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public Person(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+    }
+
+    public class PersonModel
+    {
+        public ObservableCollection<Person> Persons;
+
+        public PersonModel()
+        {
+            Persons = new ObservableCollection<Person>();
+        }
+    }
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            ObservableCollection<Person> persons = new ObservableCollection<Person>();
+            persons.Add(new Person("Alice", 23));
+            persons.Add(new Person("Sasha", 13));
+            persons.Add(new Person("Slice", 20));
+            persons.Add(new Person("Katrin", 3));
+            persons.Add(new Person("Malice", 203));
+            PersonModel personModel = new PersonModel();
+            personModel.Persons = persons;
+            SpPersons.ItemsSource = persons;
+        }
+    }
+}
